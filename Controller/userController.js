e0107b221c7dc660a08bf6ae4566434d3b6fe64f
J Praneeth <praneeth.janjanam@aceec.ac.in>
2024-11-28 14:32:04 +0530

    Updated both User Controller and User model!!!

--- Controller/userController.js
+++ Controller/userController.js
@@ -25,7 +25,7 @@ export const loginUser = async (req, res) => {
   try {
     const { emailOrRollNo, password } = req.body;
 
-    // Check if the input is a valid email or roll number
+    // Check if the input is a valid email or roll number (userID)
     const isEmail = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailOrRollNo);
     let user;
 
@@ -33,7 +33,7 @@ export const loginUser = async (req, res) => {
       // If it's an email, find the user by email
       user = await User.findOne({ email: emailOrRollNo });
     } else {
-      // If it's not an email, assume it's a roll number
+      // If it's not an email, assume it's a roll number (userID)
       user = await User.findOne({ userID: emailOrRollNo });
     }
 
@@ -47,7 +47,7 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Respond with user details (without the password)
+    // Respond with user details (without password)
     res.status(200).json({
       email: user.email,
       name: user.name,
@@ -62,24 +62,30 @@ export const loginUser = async (req, res) => {
 // export const loginUser = async (req, res) => {
 //   try {
 //     const { email, password } = req.body;
-//     const user =  await User.findOne({ email });
 
+//     // Find the user by email
+//     const user = await User.findOne({ email });
 //     if (!user) {
 //       return res.status(404).json({ message: "User not found" });
 //     }
 
-//     // Verify password using Argon2
+//     // Verify the password using Argon2
 //     const isMatch = await argon2.verify(user.password, password);
-
 //     if (!isMatch) {
 //       return res.status(401).json({ message: "Invalid credentials" });
 //     }
 
-//     res.status(200).json({ message: "Login successful" });
+//     // Respond with user details (without token)
+//     res.status(200).json({
+//       email: user.email,
+//       name: user.name,
+//       gender: user.gender,
+//     });
 //   } catch (error) {
 //     res.status(500).json({ message: "Error logging in", error: error.message });
 //   }
 // };
+
 
 // Get all users
 export const getUsers = async (req, res) => {
